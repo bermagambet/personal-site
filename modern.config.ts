a69f314072740dbbdefe04f577b45283a6fd98c2
bermagambet <bermagambet.d@gmail.com>
2025-05-28 15:32:18 +0500

    fix(pages): manifest icons

--- modern.config.ts
+++ modern.config.ts
@@ -1,13 +1,21 @@
-import { appTools, defineConfig } from '@modern-js/app-tools';
+import { appTools, defineConfig } from "@modern-js/app-tools";
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
+  html: {
+    title: "Bermagambet Duisek - CV",
+    favicon: "./src/assets/icon.svg",
+    appIcon: "./src/assets/icon.svg",
+    meta: {
+      description: "Information about me and my experience",
+    },
+  },
   runtime: {
     router: true,
   },
   plugins: [
     appTools({
-      bundler: 'rspack', // Set to 'webpack' to enable webpack
+      bundler: "rspack", // Set to 'webpack' to enable webpack
     }),
     // tailwindcssPlugin(),
   ],
@@ -15,7 +23,7 @@ export default defineConfig({
     tailwindcss: {},
     postcss: {
       postcssOptions: {
-        plugins: [require('@tailwindcss/postcss')],
+        plugins: [require("@tailwindcss/postcss")],
       },
     },
   },
