56c9b50f4604aead04ab1226555460820b7ee4d1
bermagambet <bermagambet.d@gmail.com>
2025-05-28 20:58:29 +0500

    fix(components): navbar retrieves data from url

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -1,14 +1,28 @@
-import useTitlePageStore from '@/storage';
-import React, { Fragment } from 'react';
-import type { NavBarComponent } from './types';
+import useTitlePageStore from "@/storage";
+import React, { Fragment, useEffect } from "react";
+import type { NavBarComponent } from "./types";
+
+const getAnchor = () => {
+  var currentUrl = document.URL,
+    urlParts = currentUrl.split("#");
+
+  return urlParts.length > 1 ? urlParts[1] : null;
+};
 
 const NavBar: NavBarComponent = ({ items }) => {
   const { currentTab, setCurrentTab } = useTitlePageStore();
 
+  useEffect(() => {
+    const anchor = getAnchor();
+    if (anchor) {
+      setCurrentTab(`#${anchor}`);
+    }
+  }, []);
+
   return (
     <nav className="flex gap-x-2 text-base md:text-lg border-1 bg-white border-gray-300 rounded-xl p-2.5 font-mono">
       {items.map(({ href, text }, i) => {
-        const isCurrent = currentTab === href ? 'text-sky-500' : '';
+        const isCurrent = currentTab === href ? "text-sky-500" : "";
         const isNotLast = i !== items.length - 1;
         return (
           <Fragment key={href}>
