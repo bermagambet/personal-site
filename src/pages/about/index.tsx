a23822e0c0b42c7b21dfee759c0397da377f373b
bermagambet <bermagambet.d@gmail.com>
2025-05-28 21:59:23 +0500

    fix(pages): about lighter font

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 
 const About = () => (
-  <div className="animate-fade-in flex flex-col gap-y-2 md:gap-y-8 justify-center items-center py-[10%] md:pt-0 text-xl lg:text-2xl md:h-full">
-    <p className="text-center md:w-[50%]">
+  <div className="animate-fade-in flex flex-col gap-y-2 md:gap-y-8 justify-center items-center py-[10%] md:pt-0 text-xl lg:text-2xl md:h-full font-light">
+    <p className="text-left md:w-[50%]">
       Experienced Frontend Team Lead with a PhD in progress and over 4 years in
       React, TypeScript, and modern web technologies. Proven track record at
       Halyk Bank leading teams, optimizing performance, and delivering scalable
       banking solutions that boosted revenue and reduced system downtime.
     </p>
-    <p className="text-center md:w-[50%]">
+    <p className="text-left md:w-[50%]">
       Strong background in PWA development, micro-frontend architecture, and
       caching strategies. Adept at establishing coding standards, improving
       developer workflows, and mentoring teams. Skilled in tools like Next.js,
       Redux, Zustand, Tailwind, and testing frameworks.
     </p>
-    <p className="text-center md:w-[50%]">
+    <p className="text-left md:w-[50%]">
       Passionate about building performant, maintainable systems and driving
       engineering excellence across large-scale applications.
     </p>
